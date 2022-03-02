5a792b0d15b3df3612e6b56777c952e5c15b927d
Haneul Lee <hee1149kr@naver.com>
2022-03-02 12:59:11 +0900

    Update main.js

--- module/module.exports와 exports의 차이/main.js
+++ module/module.exports와 exports의 차이/main.js
@@ -12,7 +12,7 @@ a(module, module.exports);
 
 /*
 exports 변수의 재할당은 module.exports의 내용을 변경하지 않습니다. a함수의 exports 매개변수는
-module.exports를 참조하고 있고 있는 상황에서 exports = () => {}는 modue.exports 참조에서 새로 만든 함수를 참조하게
+module.exports를 참조하고 있는 상황에서 exports = () => {}는 modue.exports 참조에서 새로 만든 함수를 참조하게
 하기때문에 module.exports의 값을 바꾸지 않지만, module.exports 에 할당을 하면 module(찐 module의 주소)의 exports의 참조하는
 값을 재할당하기 때문에 변경됩니다.
 */
